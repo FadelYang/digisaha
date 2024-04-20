3001d2f80142959b9c15c61f41c64fd12b25dbbe
fadelanumah <fadelanumah@gmail.com>
2024-04-20 23:48:16 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section("content")
+<div>
+    <p class="text-center">Hello World!!!</p>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
