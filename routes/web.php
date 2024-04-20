5382779a384d1ba26f3db86f62c3d8746bd1864c
fadelanumah <fadelanumah@gmail.com>
2024-04-21 00:12:31 +0700

    feat(email-verification): Add Resend Email Alert

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware(["auth", "verified"]);
 
 Route::get('/email/verify', function () {
     return view('auth.verify');
@@ -26,5 +26,5 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
 Route::post('/email/verification-notification', function (Request $request) {
     $request->user()->sendEmailVerificationNotification();
  
-    return back()->with('message', 'Verification link sent!');
+    return back()->with('message', 'Verification link sent to ' . $request->user()->email . ' !');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.resend');
