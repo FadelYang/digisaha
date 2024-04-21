cbc7b55016718286a67bf7b234fca759b32ed024
fadelanumah <fadelanumah@gmail.com>
2024-04-21 22:07:07 +0700

    feat(homepage): Add Hero Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,15 @@
 @extends('layouts.app')
 
-@section("content")
-<div>
-    <p class="text-center">Hello World!!!</p>
-</div>
+@section('content')
+    <div class="container">
+        <x-main-component
+            title="Bantu Digitalkan Usaha Anda"
+            subtitle="Pasarkan dan permudah operasional usaha anda dengan website profil perusahaan dan perangkat lunak manajemen usaha"
+            buttonText="Lihat Selengkapnya"
+            image="{{ asset('images/fachrizal-maulana-vaVdrH0hBg4-unsplash.jpg') }}"
+            imageAlt="Digisaha, memudahkan proses digitalisasi usaha anda"
+            :isImageRightSide="true"
+            >
+        </x-main-component>
+    </div>
 @endsection
