837f36f0faec2f118bd0468e1a7a1e2b4a077c9d
fadelanumah <fadelanumah@gmail.com>
2024-04-22 00:14:58 +0700

    feat(homepage): Add Produk dan Layanan Section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,5 +23,23 @@
             :isTitlePriorityDisplay="false"
         ></x-main-component>
         <x-section-title title="Produk dan Layanan Kami" subtitle="Landing page dan sistem manajemen usahana, bantu operasional perusahaan anda"></x-section-title>
+        <x-main-component
+            title="Hadir membantu meningkatkan usaha anda"
+            subtitle="Digisaha memiliki visi untuk membantu pengusaha untuk meningkatkan usaha mereka, melalui Digitalisasi usaha, kami berharap pengusaha dapat memperluas jangkauan usaha dan meningkatkan pendapatan."
+            buttonText="Lihat Produk Kami"
+            image="{{ asset('images/fachrizal-maulana-vaVdrH0hBg4-unsplash.jpg') }}"
+            imageAlt="Digisaha, memudahkan proses digitalisasi usaha anda"
+            :isImageRightSide="false"
+            :isTitlePriorityDisplay="false"
+        ></x-main-component>
+        <x-main-component
+            title="Hadir membantu meningkatkan usaha anda"
+            subtitle="Digisaha memiliki visi untuk membantu pengusaha untuk meningkatkan usaha mereka, melalui Digitalisasi usaha, kami berharap pengusaha dapat memperluas jangkauan usaha dan meningkatkan pendapatan."
+            buttonText="Lihat Produk Kami"
+            image="{{ asset('images/fachrizal-maulana-vaVdrH0hBg4-unsplash.jpg') }}"
+            imageAlt="Digisaha, memudahkan proses digitalisasi usaha anda"
+            :isImageRightSide="false"
+            :isTitlePriorityDisplay="false"
+        ></x-main-component>
     </div>
 @endsection
