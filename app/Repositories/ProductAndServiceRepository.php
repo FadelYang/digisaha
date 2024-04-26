c62fec794eebf7b5c554976b346b66917f61469e
fadelanumah <fadelanumah@gmail.com>
2024-04-27 02:59:09 +0700

    feat: Add Product And Service Repository

---
+++ app/Repositories/ProductAndServiceRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Interfaces\ProductAndServiceInterface;
+
+class ProductAndServiceRepository implements ProductAndServiceInterface
+{
+    public function createRegistration(array $data)
+    {
+        
+    }
+}
