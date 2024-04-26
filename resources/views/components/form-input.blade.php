ecc3eed6ec7a0c1a20778118dac217fde03ca3b6
fadelanumah <fadelanumah@gmail.com>
2024-04-27 03:49:28 +0700

    feat(input-form): Add Error Message

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -2,5 +2,12 @@
 
 <div class="{{ $classAttribute }} mb-3">
     <label for="{{ $inputName }}" class="form-label fw-bolder">{{ $inputText }}</label>
-    <input type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}" name="{{ $inputName }}" required>
+    <input type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}"
+        name="{{ $inputName }}" required>
+
+    @error('{{ $inputName }}')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>
--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -2,5 +2,12 @@
 
 <div class="{{ $classAttribute }} mb-3">
     <label for="{{ $inputName }}" class="form-label fw-bolder">{{ $inputText }}</label>
-    <textarea rows="4" type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}" name="{{ $inputName }}" required></textarea>
+    <textarea rows="4" type="{{ $inputType }}" class="form-control" id="{{ $inputName }}"
+        placeholder="{{ $placeholder }}" name="{{ $inputName }}" required></textarea>
+
+    @error('{{ $inputName }}')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
+        </span>
+    @enderror
 </div>
