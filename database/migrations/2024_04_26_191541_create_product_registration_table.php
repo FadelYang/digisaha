49a8a3f63082193222e8c3060bac33e1418b785c
fadelanumah <fadelanumah@gmail.com>
2024-04-27 02:40:11 +0700

    feat: Add Product Reistartion Table

---
+++ database/migrations/2024_04_26_191541_create_product_registration_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_registration', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->bigInteger('registrant_id');
+            $table->string('company_owner');
+            $table->string('company_email');
+            $table->string('company_phone');
+            $table->text('company_address');
+            $table->string('company_business_sector');
+            $table->string('company_business_sector_detail');
+            $table->integer('num_of_employees');
+            $table->text('monthly_income');
+
+            $table->foreign('registrant_id')->references('id')->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_registration');
+    }
+};
