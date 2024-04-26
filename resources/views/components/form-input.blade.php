2b88da8562f4278e485a9e615820afbc9e73367b
fadelanumah <fadelanumah@gmail.com>
2024-04-27 02:07:48 +0700

    fix(form-input): Fix Input Name Attribute

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -2,5 +2,5 @@
 
 <div class="{{ $classAttribute }} mb-3">
     <label for="{{ $inputName }}" class="form-label fw-bolder">{{ $inputText }}</label>
-    <input type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}" required>
+    <input type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}" name="{{ $inputName }}" required>
 </div>
--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -2,5 +2,5 @@
 
 <div class="{{ $classAttribute }} mb-3">
     <label for="{{ $inputName }}" class="form-label fw-bolder">{{ $inputText }}</label>
-    <textarea rows="4" type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}" required></textarea>
+    <textarea rows="4" type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}" name="{{ $inputName }}" required></textarea>
 </div>
