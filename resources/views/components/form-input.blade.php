f099fa85740fcb0b6c2bcd7459231496c371a80e
fadelanumah <fadelanumah@gmail.com>
2024-04-27 05:03:36 +0700

    fix(form-input): Fix Error Handling
    fix error message not apperas if some rules not meet

--- resources/views/components/form-input.blade.php
+++ resources/views/components/form-input.blade.php
@@ -2,10 +2,17 @@
 
 <div class="{{ $classAttribute }} mb-3">
     <label for="{{ $inputName }}" class="form-label fw-bolder">{{ $inputText }}</label>
-    <input type="{{ $inputType }}" class="form-control" id="{{ $inputName }}" placeholder="{{ $placeholder }}"
-        name="{{ $inputName }}" required>
+    <input 
+        type="{{ $inputType }}"
+        class="form-control @error($inputName) is-invalid @enderror"
+        id="{{ $inputName }}"
+        placeholder="{{ $placeholder }}"
+        value="{{ old($inputName) }}"
+        name="{{ $inputName }}"
+        required>
+    </input>
 
-    @error('{{ $inputName }}')
+    @error($inputName)
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
--- resources/views/components/form-textarea.blade.php
+++ resources/views/components/form-textarea.blade.php
@@ -2,10 +2,11 @@
 
 <div class="{{ $classAttribute }} mb-3">
     <label for="{{ $inputName }}" class="form-label fw-bolder">{{ $inputText }}</label>
-    <textarea rows="4" type="{{ $inputType }}" class="form-control" id="{{ $inputName }}"
-        placeholder="{{ $placeholder }}" name="{{ $inputName }}" required></textarea>
+    <textarea rows="4" type="{{ $inputType }}" class="form-control @error($inputName) is-invalid @enderror"
+        id="{{ $inputName }}" placeholder="{{ $placeholder }}" name="{{ $inputName }}" required>{{ old($inputName)}}</textarea>
 
-    @error('{{ $inputName }}')
+
+    @error($inputName)
         <span class="invalid-feedback" role="alert">
             <strong>{{ $message }}</strong>
         </span>
